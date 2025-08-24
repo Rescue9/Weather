b0f454219e0e3a515d93a959f0b3cbb80d5bc420
Rescue9 <andrew.buskov@gmail.com>
2025-08-24 15:49:56 +0000

    Fix build issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,17 @@
+import java.util.Properties
+import java.io.FileInputStream
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     id("com.android.application")
     id("kotlin-android")
 }
+
+val keystorePropertiesFile = File(rootProject.projectDir, "keystore/keystore.properties")
+val keystoreProperties = Properties().apply {
+    load(FileInputStream(keystorePropertiesFile))
+}   
 
 android {
     namespace = "com.corridor9design.weather"
@@ -14,6 +23,9 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
+        setProperty("archivesBaseName", applicationId + "-" + versionName)
+
         
         vectorDrawables { 
             useSupportLibrary = true
@@ -21,32 +33,43 @@ android {
     }
     
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     buildTypes {
-        release {
+        buildFeatures {
+            viewBinding = true
+        }
+
+        signingConfigs {
+            create("release") {
+                storeFile = File(rootProject.projectDir, "keystore/${keystoreProperties["storeFile"]}")
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
+        }
+                getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
+
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
     }
 
-    buildFeatures {
-        viewBinding = true
-        
+    tasks.withType<KotlinJvmCompile>().configureEach {
+        compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
     }
-    
-}
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions.jvmTarget = "11"
-}
+    dependencies {
 
-dependencies {
-
-
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.9.0")
+        implementation("androidx.appcompat:appcompat:1.6.1")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+        implementation("com.google.android.material:material:1.9.0")
+        implementation("androidx.multidex:multidex:2.0.1")
+    }
 }
